bbf0454dcfa2fdc24c923425aecf746f52ccc3ec
littleming <none@none.com>
2019-07-30 21:51:06 +0800

    first add

---
+++ convert.py
@@ -0,0 +1,105 @@
+import translator
+import time
+import os
+
+def convertMicroSecondToTime(microSecond):
+    '''
+    将毫秒转换为字幕时间
+    '''
+    mics = microSecond % 1000
+    if 10 <= mics < 100:
+        mics = '0' + str(mics)
+    elif 0 <= mics < 10:
+        mics = '00' + str(mics)
+    seconds = convertIntAndAddZero(microSecond // 1000 % 60)
+    minutes = convertIntAndAddZero(microSecond / 1000 // 60 % 60)
+    hours = convertIntAndAddZero(microSecond / 1000 / 60 // 60)
+    return '{}:{}:{},{}'.format(hours, minutes, seconds, mics)
+
+def convertIntAndAddZero(number):
+    '''
+    转整数以及补零
+    '''
+    number = int(number)
+    if number < 10:
+        number = '0' + str(number)
+    return number
+
+def GetCapFromFile(FileName):
+    '''
+    打开文件获取Captions字典列表
+    '''
+    with open(FileName, 'r', encoding='utf-8') as f:
+        captions = eval(f.read().replace('false','False').replace('true', 'True'))['captions']
+    return captions
+
+def ParseAndMerge(captions, translate=False):
+    '''
+    传入字幕字典列表合成srt字符串
+    '''
+    srtStrList = list()
+    for index in range(len(captions)):
+        thisCaption = captions[index]
+        srtStrList.append(str(index+1))
+        srtStrList.append('{} --> {}'.format(
+            convertMicroSecondToTime(thisCaption['startTime']),
+            convertMicroSecondToTime(thisCaption['startTime'] + thisCaption['duration'])
+            ))
+        content = thisCaption['content'].replace('\n', ' ')
+        if translate:
+            translatedStr = translator.translate(content)
+            print("({}/{}) {}".format(index+1, len(captions), translatedStr))
+            srtStrList.append(translatedStr)
+        srtStrList.append(content)
+        srtStrList.append('')
+    srtStr = '\n'.join(srtStrList)
+    return srtStr
+
+def GetNoSrtList(path):
+    files = os.listdir(path)
+    NoSrtList = list()
+    for filename in files:
+        if filename.endswith('.json') and not(os.path.exists(filename.replace('.json', '.srt'))):
+            print(filename)
+            NoSrtList.append(filename.replace('.json', '.json'))
+    return NoSrtList
+
+def is_contain_chinese(check_str):
+    """
+    判断字符串中是否包含中文
+    :param check_str: {str} 需要检测的字符串
+    :return: {bool} 包含返回True， 不包含返回False
+    """
+    for ch in check_str:
+        if u'\u4e00' <= ch <= u'\u9fff':
+            return True
+    return False
+
+def convent(FileName):
+    '''
+    将给定的json文件名转换成对应的srt文件保存在相同目录
+    '''
+    captions = GetCapFromFile(FileName)
+    # 翻译决策部分
+    print("字幕内容一瞥：" + captions[1]['content'])
+    translate = is_contain_chinese(captions[1]['content'])
+    if translate:
+        print('判断为包含中文')
+    else:
+        print('判断为不包含中文')
+    # 开始转换字幕
+    print('-------------转换字幕开始-------------')
+    srtStr = ParseAndMerge(captions, translate=not(translate))
+    print('-------------转换字幕结束-------------')
+    print("---------生成字幕内容一瞥开始----------")
+    print(srtStr[:400])
+    print("---------生成字幕内容一瞥结束----------")
+    with open(FileName.replace('.json', '.srt'), 'w', encoding='utf-8') as f:
+        f.write(srtStr)
+
+if __name__ == '__main__':
+    #path = os.getcwd()
+    path = input('请输入json所在目录：')
+    NoSrtList = map(lambda x:path + '/' + x, GetNoSrtList(path))
+    for FileName in NoSrtList:
+        convent(FileName)
---
+++ translator.py
@@ -0,0 +1,16 @@
+import urllib.request
+import sys
+
+def translate(querystr, to_l="zh", from_l="en"):
+    querystr = urllib.parse.quote(querystr)
+    C_agent = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/31.0.165063 Safari/537.36 AppEngine-Google."}
+    flag = 'class="t0">'
+    tarurl = "http://translate.google.cn/m?hl={}&sl={}&q={}".format(
+        to_l, from_l, querystr.replace(" ", "+"))
+    request = urllib.request.Request(tarurl, headers=C_agent)
+    page = str(urllib.request.urlopen(request).read().decode('utf-8'))
+    target = page[page.find(flag) + len(flag):]
+    target = target.split("<")[0]
+    return target
