e274cbc0e7e46899ee8e4f4462546999d0d93bfd
notonlyming <42286434+notonlyming@users.noreply.github.com>
2019-08-22 17:28:10 +0800

    添加异常捕获和结束语输出

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 import convert
 import SrtToJson
+import traceback
 
 message = '''
 -------------------------------------------------
@@ -11,21 +12,27 @@ Github主页：https://github.com/notonlyming
 '''
 
 if __name__ == "__main__":
-    print(message)
-    print('1.转换srt文件为json')
-    print('2.转换json文件为srt并翻译(批量操作)')
-    print('3.转换英文srt文件为中英srt')
-    choise = input('请输入序号：')
-    print('-----------------------')
-    if choise == '1':
-        SrtToJson.main()
-    elif choise == '2':
-        convert.main()
-    elif choise == '3':
-        Path = SrtToJson.main()
-        print('-----------------------正在转换{}-----------------------------'.format(Path))
-        convert.convent(Path)
-    else:
-        print('没有匹配到正确的序号')
-
-    print("\033[1;34m{}\033[0m".format('\nHave a nice day, Bye. ^_^'))
+    try:
+        print(message)
+        print('1.转换srt文件为json')
+        print('2.转换json文件为srt并翻译(批量操作)')
+        print('3.转换英文srt文件为中英srt')
+        choise = input('请输入序号：')
+        print('-----------------------')
+        if choise == '1':
+            SrtToJson.main()
+        elif choise == '2':
+            convert.main()
+        elif choise == '3':
+            Path = SrtToJson.main()
+            print('-----------------------正在转换{}-----------------------------'.format(Path))
+            convert.convent(Path)
+        else:
+            print('没有匹配到正确的序号')
+    except Exception as e:
+        traceback.print_exc()
+        input('程序发生了异常, 回车键继续。')
+    finally:
+        import time
+        print("\033[1;34m{}\033[0m".format('\nHave a nice day, Bye. ^_^\n'))
+        time.sleep(2)
