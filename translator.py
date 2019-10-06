d06986bd7b52ead8a19ccec4fce5482be292db21
littleming <lm@lm.com>
2019-10-06 11:15:44 +0800

    Add not check google translate cert code

--- translator.py
+++ translator.py
@@ -1,13 +1,15 @@
 import urllib.request
 import sys
+import ssl
 
 def translate(querystr, to_l="zh", from_l="auto"):
+    ssl._create_default_https_context = ssl._create_unverified_context
     querystr = urllib.parse.quote(querystr)
     C_agent = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) "
                 "AppleWebKit/537.36 (KHTML, like Gecko) "
                 "Chrome/31.0.165063 Safari/537.36 AppEngine-Google."}
     flag = 'class="t0">'
-    tarurl = "http://translate.google.cn/m?hl={}&sl={}&q={}".format(
+    tarurl = "https://translate.google.cn/m?hl={}&sl={}&q={}".format(
         to_l, from_l, querystr.replace(" ", "+"))
     request = urllib.request.Request(tarurl, headers=C_agent)
     page = str(urllib.request.urlopen(request).read().decode('utf-8'))
