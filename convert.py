0abb170ac1f8ddb11cb177b4ed33379cc59c7a73
crytxkb <notonlyming@qq.com>
2021-02-18 12:25:23 +0800

    更改为使用百度翻译API翻译

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 import translator
 import time
 import os
-import urllib
+import sys
 
 def convertMicroSecondToTime(microSecond):
     '''
@@ -38,41 +38,40 @@ def ParseAndMerge(captions, translate=False):
     '''
     传入字幕字典列表合成srt字符串
     '''
-    srtStrList = list()
+    srtStrList = []
+    resultList = []
     # 取出所有字幕为列表方便单独处理
     captionsStrList = list(map(lambda x:x['content'].replace('\n', ' '), captions))
     
     def translateList(captionsStrList):
         '''
         翻译给定的英文字符串列表，并把翻译结果插入到列表元素的第一行
+        由于百度翻译有6000字节的限制因此要将要翻译的东西分开。
         '''
-        print('开始翻译...')
-        # 合并为少于5000字的字符串
-        translateTempStr = ''
-        translatedStr = ''
-        lenLimits = 5000
-        splitStr = '\n\n\n\n\n'
-        for index in range(len(captionsStrList)):
-            # 一条一条加入字幕
-            # 这里计算的是转义后的字符串是否超限
-            if len(urllib.parse.quote(translateTempStr)) < lenLimits:
-                # 字幕没满，加入
-                translateTempStr += captionsStrList[index] + splitStr
-                print("({}/{}) ".format(index+1, len(captionsStrList)), end='')
+        print('原文分段...')
+        counter = 0
+        partList = list()
+        partList.append(list()) # 始终会有一部分
+        for tmpStr in captionsStrList:
+            counter += sys.getsizeof(tmpStr)
+            if counter < 6000:
+                if tmpStr != '':
+                    partList[-1].append(tmpStr)
             else:
-                # 字幕已满，翻译
-                translatedStr += translator.translate(translateTempStr)
-                # 清空并把这条加进去
-                translateTempStr = captionsStrList[index] + splitStr
-                print("({}/{}) ".format(index+1, len(captionsStrList)))
-        # 循环结束后，把剩下的翻译一下
-        translateTempStr = translateTempStr[:-len(splitStr)] 
-        translatedStr += translator.translate(translateTempStr)
-        print(translatedStr)
-        # 把翻译好的东西插入到原来的英文上面（\n 返回的是对应数目的空格）
-        translatedStr = translatedStr.split(' '*len(splitStr))
+                partList.append(list())
+                partList[-1].append(tmpStr)
+                counter = 0
+
+        print('开始翻译...')
+        for i in range(len(partList)):
+            resultList.extend(translator.translate('\n'.join(partList[i])))
+            print(f'翻译第 {i+1} 部分，共{len(partList)}部分')
+            time.sleep(1) # 接口限制，等待 1 s
+        
+        # 把翻译好的东西插入到原来的英文上面
+        print(resultList)
         for index in range(len(captionsStrList)):
-            captionsStrList[index] = translatedStr[index] + '\n' + captionsStrList[index]
+            captionsStrList[index] = resultList[index] + '\n' + captionsStrList[index]
 
     if translate:
         translateList(captionsStrList)
@@ -95,7 +94,7 @@ def GetNoSrtList(path):
     NoSrtList = list()
     for filename in files:
         if filename.endswith('.json'):
-            if not(os.path.exists(filename.replace('.json', '.srt'))):
+            if not(os.path.exists(filename.replace('.json', '.srt'))) and 'translateApi' not in filename:
                 # srt不存在
                 print(filename + '\t[ \033[0;32m{}\033[0m ]'.format('Ready'))
                 NoSrtList.append(filename)
--- translator.py
+++ translator.py
@@ -1,18 +1,63 @@
-import urllib.request
-import sys
-import ssl
+import time
+import requests
+import json
+import hashlib
+import traceback
+
+def baidu_get_url_encoded_params(query_text, lang_from, lang_to):
+    """按api调用要求拼接url
+    Args:
+        query_text: 待翻译的文本
+    Returns:
+        符合调用接口要求的参数dict
+    """
+    tmpDict = json.load(open('translateApi.json', 'r', encoding='utf-8'))
+    app_id = tmpDict['app_id']
+    app_secret = tmpDict['app_secret']
+    salt = str(round(time.time() * 1000))
+    sign_raw = app_id + query_text + salt + app_secret
+    sign = hashlib.md5(sign_raw.encode('utf8')).hexdigest()
+    params = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'q': query_text,
+        'from': lang_from,
+        'to': lang_to,
+        'appid': app_id,
+        'salt': salt,
+        'sign': sign
+    }
+    return params
 
 def translate(querystr, to_l="zh", from_l="auto"):
-    ssl._create_default_https_context = ssl._create_unverified_context
-    querystr = urllib.parse.quote(querystr)
-    C_agent = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) "
+    # 发送的请求头
+    header = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) "
                 "AppleWebKit/537.36 (KHTML, like Gecko) "
                 "Chrome/31.0.165063 Safari/537.36 AppEngine-Google."}
-    flag = 'class="t0">'
-    tarurl = "https://translate.google.cn/m?hl={}&sl={}&q={}".format(
-        to_l, from_l, querystr.replace(" ", "+"))
-    request = urllib.request.Request(tarurl, headers=C_agent)
-    page = str(urllib.request.urlopen(request).read().decode('utf-8'))
-    target = page[page.find(flag) + len(flag):]
-    target = target.split("<")[0]
-    return urllib.parse.unquote(target).replace('&gt;', '>').replace('&lt;', '<')
+
+    # get 请求的参数(未来可能发生变动)
+    params = baidu_get_url_encoded_params(querystr, 'auto', 'zh')
+    
+    base_url = 'https://fanyi-api.baidu.com/api/trans/vip/translate'
+    # 发送请求，出错重试机制
+    for i in range(3):
+        retry = False
+        try:
+            response = requests.post(base_url, headers=header, data=params)
+            # 检查返回状态码
+            if not response.status_code == 200:
+                raise Exception(f'error res code:{response.status_code}')
+
+            resJson = json.loads(response.text) # 解析返回的文本
+            resList = [i['dst'] for i in resJson['trans_result']]
+            print(resList) # 调试输出返回的json
+
+            # 检查是否需要重试
+            if not retry:
+                break
+        except:
+            traceback.print_stack()
+            print(f'\n出错啦，重试剩余：{3-i}')
+            time.sleep(1.5) # 延迟1.5s
+            retry = True
+
+    return resList
