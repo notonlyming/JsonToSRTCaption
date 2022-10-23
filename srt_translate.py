185a6d4c9eae0ad7a649f652c696e28118194eba
unknown <crytx@lmnot.home.littleming.xyz>
2022-10-23 20:02:29 +0800

    优化srt翻译流程，不产生多余的json

--- srt_translate.py
+++ srt_translate.py
@@ -9,18 +9,19 @@ def main():
     files_list = os.listdir(base_path)
     srt_files = filter(lambda x: x.endswith('.srt'), files_list)
     json_files = tuple(filter(lambda x: x.endswith('.json'), files_list))
-    srt_files_no_translate = tuple(filter(lambda x: (x[:-4]+'.json') not in json_files, srt_files))
     if input("continue(y/n):") == 'y':
-        # 转换没有转换成json的文件
-        for srt_file in srt_files_no_translate:
-            Path = SrtToJson.main(base_path + '/' + srt_file[:-4])
-            print('正在转换: {}'.format(srt_file))
-        # 换json为srt，会自动判断是否已翻译
-        for json_file in json_files:
-            print(f'翻译{json_file}')
-            path = base_path + '/' + json_file
-            with open(f'{base_path}/{json_file[:-5] + ".srt"}', 'r', encoding='utf-8') as f:
+        # 遍历所有srt文件
+        for srt_file in srt_files:
+            print(f'翻译{srt_file}')
+            # 判断是否需要翻译
+            with open(f'{base_path}/{srt_file}', 'r', encoding='utf-8') as f:
                 if convert.is_contain_chinese(f.read()):
                     print("包含中文，跳过……")
-                else:
-                    convert.convert(path)
+                    continue
+
+            # 需要翻译，转换成json的文件
+            print('{}转换为json'.format(srt_file))
+            path_json = SrtToJson.main(base_path + '/' + srt_file[:-4])
+            convert.convert(path_json)
+            print('清理临时json……')
+            os.remove(path_json)
